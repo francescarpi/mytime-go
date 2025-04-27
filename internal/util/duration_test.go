dba4c3049ad93f8c11a8f2310894f9474b603075
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-04-27 17:14:32 +0200

    add tests for duration

---
+++ internal/util/duration_test.go
@@ -0,0 +1,26 @@
+package util
+
+import "testing"
+
+func TestHumanizeDuration(t *testing.T) {
+	tests := []struct {
+		duration int
+		expected string
+	}{
+		{0, "0m"},
+		{1, "0m"},
+		{2, "0m"},
+		{60, "1m"},
+		{61, "1m"},
+		{120, "2m"},
+		{3600, "1h"},
+		{3661, "1h1m"},
+	}
+
+	for _, test := range tests {
+		result := HumanizeDuration(test.duration)
+		if result != test.expected {
+			t.Errorf("Expected %v for duration %v, got %v", test.expected, test.duration, result)
+		}
+	}
+}
